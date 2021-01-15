fe4f3e8f025189e9903203835631daab139e6450
Irina <ira.verbilovich@mail.ru>
2021-01-15 18:55:11 +0300

    added broken toggle

--- source/js/app.js
+++ source/js/app.js
@@ -4,3 +4,21 @@ checkbox.addEventListener('change', () => {
    //change the theme of the page
    document.body.classList.toggle('dark');
 });
+
+/* Menu nav toggle */
+$("#nav_toggle").on("click", function(event) {
+   event.preventDefault();
+
+   $(this).toggleClass("active");
+   $("#nav").toggleClass("active");
+});
+
+/* Collapse */
+$("[data-collapse]").on("click", function(event) {
+   event.preventDefault();
+
+   var $this = $(this),
+      blockId = $this.data('collapse');
+
+   $this.toggleClass("active");
+});
